70e8f72c3417909b667207549947e548c14d4f2e
PeterHUistyping <2447276617@qq.com>
2022-11-10 22:01:59 +0000

    Updated Hello1

--- Concurrent/hello.cpp
+++ Concurrent/hello.cpp
@@ -1,13 +1,25 @@
 #include<iostream>
 #include <thread>
 using namespace std;
-void test(int x){
+// void test(int x){
+//     cout<<"Thread"<<endl;
+//     cout<<"Arg: "<<x<<endl;
+// }
+int main(){
+    
+    auto lambda=[](int x){
     cout<<"Thread"<<endl;
     cout<<"Arg: "<<x<<endl;
-}
-int main(){
-    std::thread myThread(&test,100);
+    };
+    // std::thread myThread(&test,100);
+    std::thread myThread(lambda,100);
     myThread.join();
+    // The function returns when the thread execution has completed.
+
+    // This synchronizes the moment this function returns with the completion of 
+    // all the operations in the thread: This blocks the execution of the thread
+    //  that calls this function until the function called on construction 
+    //  returns (if it hasn't yet).
     cout<<"Main Thread "<<endl;
     return 0;
 }
